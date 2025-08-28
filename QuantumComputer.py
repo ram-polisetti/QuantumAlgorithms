1d7d24ab926b44172e830dcc477ae30eb9c81ebd
rampolisetti <saitejapolisetti284@gmail.com>
2025-08-28 10:54:30 -0400

    updated gitignore

--- QuantumComputer.py
+++ QuantumComputer.py
@@ -1,5 +1,7 @@
 from qiskit import *
-from qiskit_ibm_runtime import QiskitRuntimeService, Sampler
+from qiskit_ibm_runtime import QiskitRuntimeService
+import matplotlib.pyplot as plt
+from qiskit.visualization import plot_histogram
 import time
 
 with open('ibmapi.txt', 'r') as file:
@@ -19,3 +21,29 @@ for backend in service.backends():
     status = backend.status().operational
     pending_jobs = backend.status().pending_jobs
     print(f"{name:20} | Qubits: {qubits} | Operational: {status} | Pending Jobs: {pending_jobs}")
+
+
+backend = service.backend("ibm_brisbane")
+transpiled_qc = transpile(circuit, backend=backend)
+
+
+sampler = Sampler(backend)
+# need to pass a list of circuits to `sampler.run()`, even if you have only one circuit.
+
+job = sampler.run([transpiled_qc], shots=1024)
+while not job.done():
+    print(f"Job is still running. Current status: {job.status()}")
+    time.sleep(2)  # Wait for 10 seconds before checking again
+
+job.wait_for_final_state()
+print(f"Job completed with status: {job.status()}")
+
+result = job.result()
+
+bitvals = result[0].data.c
+
+counts = bitvals.get_counts()
+
+print(counts)
+
+plot_histogram(counts)
